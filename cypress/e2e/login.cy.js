9ba7899ea22c3ca6c0e58db6f3fcdaa26e8b12ef
viniciusxdxd <contatoviniciussf@gmail.com>
2023-05-04 00:48:52 -0300

    meu primeiro commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso', () =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click('')
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário ou senha inválidos', () =>{
+
+    })
+})
